06e3946d2701b393b55b7025e6c52b5c679886b0
aodq <dtoadq@gmail.com>
2019-02-02 05:03:32 -0500

    Prints instead of comments, moves cimgui.so

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/sh
 
-# update our externals/dependencies
+echo " -- Updating dtoavk external dependencies"
 git submodule update --recursive --init
 
-# install cimgui
+echo " -- Installing cimgui for local project repository"
 cd external/cimgui/
 make -j$(nproc)
-echo "Installing cimgui.so to local project repository"
-cp cimgui.so ../../libcimgui.so
+echo " -- Attempt move of cimgui.so to local project repository"
+mv cimgui.so ../../libcimgui.so
 
-# add neobc and dtoavk-bindings to dub environment
+echo " -- Adding neobc and dtoavk-bindings to dub environment"
 cd ../neobc
 dub add-local .
 cd ../dtoavk-bindings
 dub add-local .
 
-# go back to project root
 cd ../../
-echo "Should be able to build, try `dub build --compiler=dmd --build=release`"
+echo "Should be able to build, try: dub build --compiler=dmd --build=release"
